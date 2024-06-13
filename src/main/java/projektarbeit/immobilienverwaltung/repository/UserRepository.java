a1f1723aa38e27945b7332bf77a16e2a5998c17f
Leander <leanderlange01@gmx.de>
2024-06-13 18:19:27 +0200

    Repository für den Zugriff auf die User

---
+++ src/main/java/projektarbeit/immobilienverwaltung/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package projektarbeit.immobilienverwaltung.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import projektarbeit.immobilienverwaltung.model.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
