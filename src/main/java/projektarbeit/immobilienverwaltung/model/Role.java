27af59ded3ba1f5a4462dce53a1c2d7d05202155
Leander <leanderlange01@gmx.de>
2024-06-13 18:16:00 +0200

    Datenmodell für die Rollen

---
+++ src/main/java/projektarbeit/immobilienverwaltung/model/Role.java
@@ -0,0 +1,45 @@
+package projektarbeit.immobilienverwaltung.model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+
+    public long getId(){
+        return id;
+    }
+
+    public void setId(Long id){
+        this.id = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public Set<User> getUsers(){
+        return users;
+    }
+
+    public void setUsers(Set<User> users){
+        this.users = users;
+    }
+}
