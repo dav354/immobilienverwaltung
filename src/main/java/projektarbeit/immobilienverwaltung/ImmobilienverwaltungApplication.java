9eadfad036e6cf248d5306dca348268f0c3570c9
Leander <leanderlange01@gmx.de>
2024-06-13 18:15:29 +0200

    Annotations hinzugefügt

--- src/main/java/projektarbeit/immobilienverwaltung/ImmobilienverwaltungApplication.java
+++ src/main/java/projektarbeit/immobilienverwaltung/ImmobilienverwaltungApplication.java
@@ -2,8 +2,12 @@ package projektarbeit.immobilienverwaltung;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "projektarbeit.immobilienverwaltung.model")
+@EnableJpaRepositories(basePackages = "projektarbeit.immobilienverwaltung.repository")
 public class ImmobilienverwaltungApplication {
 
     public static void main(String[] args) {
