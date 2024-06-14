5738b096eccf395e896e7f93b7c1753c8c9ee439
Leander <leanderlange01@gmx.de>
2024-06-14 21:06:28 +0200

    JavaDoc hinzugefügt

--- src/main/java/projektarbeit/immobilienverwaltung/ImmobilienverwaltungApplication.java
+++ src/main/java/projektarbeit/immobilienverwaltung/ImmobilienverwaltungApplication.java
@@ -5,11 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+/**
+ * Hauptklasse der Immobilienverwaltungsanwendung.
+ * Diese Klasse konfiguriert und startet die Spring Boot-Anwendung.
+ */
 @SpringBootApplication
 @EntityScan(basePackages = "projektarbeit.immobilienverwaltung.model")
 @EnableJpaRepositories(basePackages = "projektarbeit.immobilienverwaltung.repository")
 public class ImmobilienverwaltungApplication {
 
+    /**
+     * Der Haupteinstiegspunkt der Spring Boot-Anwendung.
+     *
+     * @param args die Befehlszeilenargumente.
+     */
     public static void main(String[] args) {
         SpringApplication.run(ImmobilienverwaltungApplication.class, args);
     }
