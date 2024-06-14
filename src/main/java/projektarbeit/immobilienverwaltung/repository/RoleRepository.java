3cbbc10ff9ac20b55069f76d8651440d088d378b
Leander <leanderlange01@gmx.de>
2024-06-14 14:41:20 +0200

    JavaDoc hinzugefügt

--- src/main/java/projektarbeit/immobilienverwaltung/repository/RoleRepository.java
+++ src/main/java/projektarbeit/immobilienverwaltung/repository/RoleRepository.java
@@ -5,6 +5,17 @@ import projektarbeit.immobilienverwaltung.model.Role;
 
 import java.util.Optional;
 
+/**
+ * Repository für die Verwaltung der Rollen-Entitäten.
+ * Erweitert JpaRepository, um CRUD-Operationen bereitzustellen.
+ */
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    /**
+     * Findet eine Rolle anhand des Rollennamens.
+     *
+     * @param name der Name der zu findenden Rolle.
+     * @return ein Optional, das die gefundene Rolle enthält, oder leer ist, wenn keine Rolle mit dem angegebenen Namen gefunden wurde.
+     */
     Optional<Role> findByName(String name);
 }
