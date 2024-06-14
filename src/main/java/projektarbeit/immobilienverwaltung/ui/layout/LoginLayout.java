56f3d18fd826f3d226c9ba1eb5a3e60e2c393eab
Leander <leanderlange01@gmx.de>
2024-06-14 14:40:11 +0200

    JavaDoc hinzugefügt

--- src/main/java/projektarbeit/immobilienverwaltung/ui/layout/LoginLayout.java
+++ src/main/java/projektarbeit/immobilienverwaltung/ui/layout/LoginLayout.java
@@ -6,10 +6,18 @@ import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
 import com.vaadin.flow.theme.lumo.Lumo;
 
+/**
+ * Diese Klasse stellt das Layout für die Login-Seite der Immobilienverwaltung dar.
+ * Sie definiert das allgemeine Erscheinungsbild und die Anordnung der Login-Ansicht.
+ */
 @CssImport("./styles/shared-styles.css")
 @AnonymousAllowed
 public class LoginLayout extends VerticalLayout implements RouterLayout {
 
+    /**
+     * Konstruktor für die LoginLayout-Klasse.
+     * Initialisiert das Layout und aktiviert den Darkmode.
+     */
     public LoginLayout() {
         setSizeFull();
         setAlignItems(Alignment.CENTER);
@@ -19,6 +27,9 @@ public class LoginLayout extends VerticalLayout implements RouterLayout {
         enableDarkMode(); // Enable dark mode
     }
 
+    /**
+     * Aktiviert den Darkmode für das Layout.
+     */
     private void enableDarkMode() {
         getElement().getThemeList().add(Lumo.DARK);
     }
