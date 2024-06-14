3e5188e1f220508c33c321ce41e6c906a8530243
Leander <leanderlange01@gmx.de>
2024-06-14 12:55:12 +0200

    Erweiterung um eine Mthode um einen Useer anhand seines Admins zu finden

--- src/main/java/projektarbeit/immobilienverwaltung/repository/UserRepository.java
+++ src/main/java/projektarbeit/immobilienverwaltung/repository/UserRepository.java
@@ -3,8 +3,11 @@ package projektarbeit.immobilienverwaltung.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import projektarbeit.immobilienverwaltung.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    List<User> findByCreatedByAdmin(User admin);
 }
