294d6f0b44e02fdd64aefb006d6efc7e3d0c4425
David <david@glaesle.cloud>
2024-07-07 22:59:06 +0200

    update javadoc

--- frontend/generated/flow/generated-flow-imports.js
+++ frontend/generated/flow/generated-flow-imports.js
@@ -1,9 +1,3 @@
-import { injectGlobalCss } from 'Frontend/generated/jar-resources/theme-util.js';
-
-import { css, unsafeCSS, registerStyles } from '@vaadin/vaadin-themable-mixin';
-import $cssFromFile_0 from 'Frontend/styles/shared-styles.css?inline';
-
-injectGlobalCss($cssFromFile_0.toString(), 'CSSImport end', document);
 import '@vaadin/common-frontend/ConnectionIndicator.js';
 import '@vaadin/polymer-legacy-adapter/style-modules.js';
 import '@vaadin/accordion/theme/lumo/vaadin-accordion.js';
