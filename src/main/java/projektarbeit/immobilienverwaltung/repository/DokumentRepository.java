3effc8eb1ec1c795ca25d420358bff39ad7e1945
David <david@glaesle.cloud>
2024-07-09 15:00:03 +0200

    use faster joins on dokument

--- src/main/java/projektarbeit/immobilienverwaltung/repository/DokumentRepository.java
+++ src/main/java/projektarbeit/immobilienverwaltung/repository/DokumentRepository.java
@@ -2,6 +2,7 @@ package projektarbeit.immobilienverwaltung.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 import projektarbeit.immobilienverwaltung.model.Dokument;
 import projektarbeit.immobilienverwaltung.model.Mieter;
 import projektarbeit.immobilienverwaltung.model.Wohnung;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface DokumentRepository extends JpaRepository<Dokument, Long> {
+    @Query("SELECT d FROM Dokument d JOIN FETCH d.wohnung WHERE d.wohnung = :wohnung")
     List<Dokument> findByWohnung(Wohnung wohnung);
+
+    @Query("SELECT d FROM Dokument d JOIN FETCH d.mieter WHERE d.mieter = :mieter")
     List<Dokument> findByMieter(Mieter mieter);
 }
