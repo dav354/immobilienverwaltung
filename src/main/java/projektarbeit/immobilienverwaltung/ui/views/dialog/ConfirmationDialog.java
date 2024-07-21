5642ab835338f6dfd3846430124fdaa65326a64d
David <david@glaesle.cloud>
2024-07-21 11:57:30 +0200

    move buttons to the bottom

--- src/main/java/projektarbeit/immobilienverwaltung/ui/views/dialog/ConfirmationDialog.java
+++ src/main/java/projektarbeit/immobilienverwaltung/ui/views/dialog/ConfirmationDialog.java
@@ -3,7 +3,9 @@ package projektarbeit.immobilienverwaltung.ui.views.dialog;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import projektarbeit.immobilienverwaltung.service.ConfigurationService;
 import projektarbeit.immobilienverwaltung.ui.layout.DialogLayout;
 
@@ -30,12 +32,16 @@ public class ConfirmationDialog extends DialogLayout {
      */
     public ConfirmationDialog(String message, ConfirmationListener confirmationListener, ConfigurationService configurationService) {
         super(configurationService);
-
-        // Einrichten des DialogLayouts
+        // Setzen des Dialog-Layouts
         setDialogLayout();
 
-        // Hinzufügen des Nachrichtentexts zum Dialog
-        add(new Text(message));
+        // Hauptlayout für den Dialog erstellen
+        VerticalLayout mainLayout = new VerticalLayout();
+        mainLayout.setPadding(true);
+        mainLayout.setSpacing(true);
+
+        // Hinzufügen des Nachrichtentexts zum Hauptlayout
+        mainLayout.add(new Text(message));
 
         // Erstellen der "Löschen"-Schaltfläche und Hinzufügen eines Klick-Listeners zum Auslösen des Bestätigungslisteners
         Button confirmButton = new Button("Löschen", event -> {
@@ -49,8 +55,17 @@ public class ConfirmationDialog extends DialogLayout {
         cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY); // Hinzufügen des tertiären Themas zur "Abbrechen"-Schaltfläche
 
         // Anordnen der Schaltflächen in einem horizontalen Layout
-        HorizontalLayout buttons = new HorizontalLayout(confirmButton, cancelButton);
-        add(buttons); // Hinzufügen des Schaltflächenlayouts zum Dialog
+        HorizontalLayout buttonLayout = new HorizontalLayout(confirmButton, cancelButton);
+        buttonLayout.setWidthFull();
+        buttonLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.END);
+
+        // Schaltflächenlayout zum Hauptlayout hinzufügen
+        mainLayout.add(buttonLayout);
+        mainLayout.setSizeFull();
+        mainLayout.expand(buttonLayout);
+
+        // Hauptlayout dem Dialog hinzufügen
+        add(mainLayout);
     }
 
     /**
